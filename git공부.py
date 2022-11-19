b60898e7ae237bda023b175c4d60cc79854a5298
giba.k <giba.k@kakaocorp.com>
2022-11-19 14:50:48 +0900

    third commit

---
+++ git공부.py
@@ -0,0 +1,18 @@
+git restore : add한거를 취소
+git checkout . : 최신의 커밋으로 되돌림
+git checkout [filename] :  staged 파일을 가져오기
+git commit -am "sdfds" : add & commit 한꺼번에
+
+git fetch origin : 변경 사항을 가져옴(단 동기화는 아님)
+이후 git checkout origin/master를 통해 변경 사항 확인가능
+-> 다시 돌아오려면 git checkout master
+
+git remote -v 
+
+
+master ----------------------------------
+
+logan---
+giba
+havery 
+
